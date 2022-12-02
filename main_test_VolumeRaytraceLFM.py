9c3818700a72babfea57a13343b90b9679a8516f
Josue Page Vizcaino <pv.josue@gmail.com>
2022-12-02 11:44:04 -0500

    Added timing with different methods

--- main_test_VolumeRaytraceLFM.py
+++ main_test_VolumeRaytraceLFM.py
@@ -3,6 +3,7 @@ from VolumeRaytraceLFM.birefringence_implementations import *
 
 from ray import *
 from jones import *
+import time
 
 # Set objective info
 optic_config = OpticConfig()
@@ -16,26 +17,29 @@ optic_config.camera_config.sensor_pitch = 6.5
 optic_config.mla_config.pitch = optic_config.mla_config.n_pixels_per_mla * optic_config.camera_config.sensor_pitch
 optic_config.mla_config.n_mlas = 100
 
-optic_config.volume_config.volume_shape = [3, 11, 11]
+optic_config.volume_config.volume_shape = [111, 111, 111]
 optic_config.volume_config.voxel_size_um = [1,] + 2*[optic_config.mla_config.pitch / optic_config.PSF_config.M]
 optic_config.volume_config.volume_size_um = np.array(optic_config.volume_config.volume_shape) * np.array(optic_config.volume_config.voxel_size_um)
 
 # Create a Birefringent Raytracer
 BF_raytrace = BirefringentRaytraceLFM(optic_config=optic_config, members_to_learn=[])
+
 # Compute the rays and use the Siddon's algorithm to compute the intersections with voxels.
 # If a filepath is passed as argument, the object with all its calculations get stored/loaded from a file
-
+startTime = time.time()
 BF_raytrace = BF_raytrace.compute_rays_geometry() #'test_ray_geometry'
+executionTime = (time.time() - startTime)
+print('Ray-tracing time in seconds: ' + str(executionTime))
 
 # Create a Birefringent volume, with random 
 volume = BF_raytrace.init_volume(init_mode='random')
 
 
 # Single voxel
-if False:
+if True:
     my_volume = BF_raytrace.init_volume(init_mode='zeros')
     offset = 0
-    my_volume.voxel_parameters[:, BF_raytrace.vox_ctr_idx[0], BF_raytrace.vox_ctr_idx[1]+offset, BF_raytrace.vox_ctr_idx[2]+offset] = torch.tensor([0.25, -0.5, 0.5, 0])
+    my_volume.voxel_parameters[:, BF_raytrace.vox_ctr_idx[0], BF_raytrace.vox_ctr_idx[1]+offset, BF_raytrace.vox_ctr_idx[2]+offset] = torch.tensor([1, -0.5, 0.5, 0])
 else: # whole plane
     my_volume = BF_raytrace.init_volume(init_mode='1planes')
 # my_volume.plot_volume_plotly(opacity=0.1)
@@ -44,7 +48,7 @@ else: # whole plane
 ray_enter, ray_exit, ray_diff = rays_through_vol(optic_config.mla_config.n_pixels_per_mla, optic_config.PSF_config.NA, optic_config.PSF_config.ni, BF_raytrace.volCtr)
 
 # Comparing ray_enter/exit/diff with BF_raytrace.ray_entry/exit/direction... All the same :) 
-import time
+
 startTime = time.time()
 ret_image, azim_image = ret_and_azim_images(ray_enter, ray_exit, ray_diff, optic_config.mla_config.n_pixels_per_mla, my_volume.voxel_parameters, optic_config)
 executionTime = (time.time() - startTime)
