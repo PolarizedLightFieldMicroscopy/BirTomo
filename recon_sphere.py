19adea435829dec14bfee54f8d7c7a1dcbc92a36
Geneva Schlafly <gschlafly@uchicago.edu>
2024-01-03 19:45:32 -0600

    Box out initial birefringence based on sphere location

--- recon_sphere.py
+++ recon_sphere.py
@@ -73,14 +73,14 @@ def recon_sphere():
     visualize_volume(reconstructor.volume_pred, reconstructor.optical_info)
 
 
-def recon_continuation():
+def recon_continuation(init_vol_path):
     optical_info = setup_optical_parameters(
-        "config_settings/optical_config_sphere.json")
-    foward_img_str = 'sphere6_thick1_31mla_17pix.npy'
+        "config_settings/optical_config_sphere_large_vol.json")
+    foward_img_str = 'sphere6_thick2_31mla_17pix.npy'
     volume_GT = BirefringentVolume(
         backend=BACKEND,
         optical_info=optical_info,
-        volume_creation_args=volume_args.sphere_args6
+        volume_creation_args=volume_args.sphere_args6_thick
     )
     # visualize_volume(volume_GT, optical_info)
     ret_image_meas = np.load(os.path.join(
@@ -91,10 +91,9 @@ def recon_continuation():
     # reconstruction
     recon_optical_info = optical_info.copy()
     iteration_params = setup_iteration_parameters(
-        "config_settings/iter_config_sphere.json")
-    sphere_path = "volumes/2024-01-02_23-26-15/volume_ep_300_threshold_0.002_bir.h5"
+        "config_settings/iter_config_sphere_large_vol.json")
     initial_volume = BirefringentVolume.init_from_file(
-        sphere_path, BackEnds.PYTORCH, recon_optical_info)
+        init_vol_path, BackEnds.PYTORCH, recon_optical_info)
     visualize_volume(initial_volume, recon_optical_info)
 
     recon_directory = create_unique_directory("reconstructions")
@@ -110,65 +109,6 @@ def recon_continuation():
     reconstructor.reconstruct(output_dir=recon_directory)
     visualize_volume(reconstructor.volume_pred, reconstructor.optical_info)  
 
-def zero_near_zero(tensor, threshold):
-    """
-    Set elements of the tensor to zero if they are within a specified
-    threshold of zero.
-
-    Args:
-        tensor (torch.Tensor): The input tensor.
-        threshold (float): The threshold for determining "close to zero".
-
-    Returns:
-        torch.Tensor: The modified tensor with elements close to zero
-                      set to zero.
-    """
-    # Identify elements within the threshold range
-    close_to_zero = torch.logical_and(tensor > -threshold, tensor < threshold)
-
-    # Set these elements to zero
-    tensor[close_to_zero] = 0
-
-    return tensor
-
-def identify_close_to_zero(tensor, threshold):
-    """
-    Identify elements of the tensor that are within a specified
-    threshold of zero.
-
-    Args:
-        tensor (torch.Tensor): The input tensor.
-        threshold (float): The threshold for determining "close to zero".
-
-    Returns:
-        torch.Tensor: The modified tensor with elements close to zero
-                      set to zero.
-    """
-    # Identify elements within the threshold range
-    close_to_zero = torch.logical_and(tensor > -threshold, tensor < threshold)
-
-    return close_to_zero
-
-def zero_near_zero_voxels(volume : BirefringentVolume, threshold):
-    """Set voxels of the volume to zero if they are within a specified
-    threshold of zero."""
-    volume.Delta_n.requires_grad_(False)
-    volume.optic_axis.requires_grad_(False)
-    close_to_zero = identify_close_to_zero(volume.Delta_n, threshold)
-    volume.Delta_n[close_to_zero] = 0
-    # volume.optic_axis[:, close_to_zero] = 0
-
-    return volume
-
-def threshold_and_save_volume(input_vol_path, output_vol_path, optical_info, threshold):
-    """Load a volume, set voxels close to zero to zero, and save the volume."""
-    input_volume = BirefringentVolume.init_from_file(
-        input_vol_path, BackEnds.PYTORCH, optical_info)
-    output_volume = zero_near_zero_voxels(input_volume, threshold)
-    my_description = f"Volume {input_vol_path} thresholded at {threshold}"
-    output_volume.save_as_file(output_vol_path, my_description)
-    print(f"Saved volume to {output_vol_path}.")
-
 if __name__ == '__main__':
     # ths = 0.002
     # optical_info = setup_optical_parameters("config_settings/optical_config_sphere.json")
@@ -177,5 +117,7 @@ if __name__ == '__main__':
     #     f"volumes/2024-01-02_23-26-15/volume_ep_300_threshold_{ths}_bir.h5",
     #     optical_info, ths
     # )
-    # recon_continuation()
-    recon_sphere()
+    # sphere_path = "volumes/2024-01-02_23-26-15/volume_ep_300_threshold_0.002_bir.h5"
+    sphere_path = "volumes/random_volume_box_sphere6_thick.h5"
+    recon_continuation(sphere_path)
+    # recon_sphere()
