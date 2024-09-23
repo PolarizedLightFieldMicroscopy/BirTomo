65da0e221c3ba6fbd2f0e33d537c5106415bafe7
Trevor Anderberg <tanderberg@uchicago.edu>
2024-09-23 16:12:51 -0400

    add methods for loading info from a volume, and visualizing it

--- src/VolumeRaytraceLFM/visualization/napari_viz.py
+++ src/VolumeRaytraceLFM/visualization/napari_viz.py
@@ -1,29 +1,55 @@
 import napari
 # from napari_animation import Animation
 import numpy as np
-import matplotlib as mpl
+# import matplotlib as mpl
 import vispy as vs
 
 
-def load_from_numpy(file_path):
+def load_from_npz(file_path):
     '''Load the birfingence and optic axis data from a npz file'''
     # file_path = r"samples\celery_30_60_140_from2024-05-15_17-40-05_random1.npz"
     npz = np.load(file_path)
     birefringence = npz['birefringence']
-    optic_axis = move_comps_to_end(npz['optic_axis'])
+    optic_axis = npz['optic_axis']
     return birefringence, optic_axis
+
+def load_np_from_birVol(volume):
+        '''Gets the data from the volume we will need for plotting in napari'''
+        delta_n = volume.get_delta_n() * 1
+        optic_axis = volume.get_optic_axis() * 1
+        optical_info = volume.optical_info
+        # Check if this is a torch tensor
+        if not isinstance(delta_n, np.ndarray):
+            try:
+                delta_n = delta_n.cpu().detach().numpy()
+                optic_axis = optic_axis.cpu().detach().numpy()
+            except:
+                pass
+
+        
+        if "voxel_size_um" not in optical_info:
+            optical_info["voxel_size_um"] = [1, 1, 1]
+            print(
+                "Notice: 'voxel_size_um' was not found in optical_info. Size of [1, 1, 1] assigned."
+            )
+
+        um_per_pix = tuple(optical_info["voxel_size_um"])
+        
+        return delta_n,optic_axis,um_per_pix
 
 def bir_threshold(optic_axis,birefringence, threshold = .001):
     ## make adjust data for ploting in napari
     # no_birefringence = np.abs(birefringence)<=np.percentile(np.abs(birefringence),95)
     no_birefringence = np.abs(birefringence)<= threshold
-    optic_axis[no_birefringence] = 0
+    optic_axis[:,no_birefringence] = 0
     return optic_axis
 
 def move_comps_to_end(optic_axis):
     return np.moveaxis(optic_axis, 0, -1)
     
 def to_Nx2xD(img_like_vect, birefringence = None, omit_zeros = False):
+    '''Converts a image like vector field into a list of N vector and position pairs. If omit zeros is true, birefringence != 0  is used as a mask. The values of birefringence are put into a features dic for napari to read in to color the vectors.'''
+    img_like_vect = move_comps_to_end(img_like_vect)
     dims = np.array(img_like_vect.shape)
     N = np.prod(dims[:-1])
     pos_vect = np.zeros((N,2,dims[-1]))
@@ -72,6 +98,8 @@ def to_Nx2xD(img_like_vect, birefringence = None, omit_zeros = False):
         return pos_vect, features
 
 def to_Nx2xD_z_slice(img_like_vect,birefringence = None, omit_zeros = False):
+    '''Converts a image like vector field into a list of N vector and position pairs and adds another dimension so we can view slices in along the z axis. At each position on the new axis, only vectors with the same z value are added. We can scroll through this axis in napari to change which z plane is visible. If omit zeros is true, birefringence != 0  is used as a mask. The values of birefringence are put into a features dic for napari to read in to color the vectors.'''
+    img_like_vect = move_comps_to_end(img_like_vect)
     dims = np.array(img_like_vect.shape)
     N = np.prod(dims[:-1])
     pos_vect = np.zeros((N,2,dims[-1]+1))
@@ -139,10 +167,13 @@ def open_viewer():
 def plot_all_vectors(birefringence, optic_axis, viewer = None, colorlims = None, colormap = 'viridis_r', um_per_pix = 1.7333):
         
     # reshape the image like numpy arrays into a list of vectors and positions that napari wants
+    print("Reshaping the birefringence data for napari...")
     all_vects , all_vects_bir= to_Nx2xD(optic_axis, birefringence=birefringence, omit_zeros=True)
-    
+    # print('Done!')
+
     # open up a new viewer if we dont have a current viewer
     if viewer is None:
+        print('Creating a new napari viewer...')
         viewer = open_viewer()
     
     # set the
@@ -164,19 +195,21 @@ def plot_all_vectors(birefringence, optic_axis, viewer = None, colorlims = None,
     elif isinstance(um_per_pix, (float,int)):
         scale3 = (um_per_pix,um_per_pix,um_per_pix)
     else:
-        raise TypeError(f"um_per_pix is niether a tuple with len 3 or a float or int. It is a {type(um_per_pix)} and I'm not sure what to do with this")
-
+        raise TypeError(f"um_per_pix is niether a tuple with len 3 or a float or int. It is a {type(um_per_pix)}, {print(um_per_pix)} and I'm not sure what to do with this")
+    print('Adding vectors to the viewer...')
     all_vectors =  viewer.add_vectors(all_vects,features=all_vects_bir,edge_color='bir',vector_style='line',scale=scale3, edge_contrast_limits=colorlims, edge_colormap=vs_cmap, edge_width=.3,length=1,opacity=.75,blending='opaque')
-    return all_vectors
+    return viewer,all_vectors
 
 
 def plot_sliced_vectors(birefringence, optic_axis, viewer = None, colorlims = None, colormap = 'viridis_r', um_per_pix = 1.7333):
         
     # reshape the image like numpy arrays into a list of vectors and positions that napari wants
+    print("Reshaping the birefringence data for napari...")
     sliced_vects , sliced_bir= to_Nx2xD_z_slice(optic_axis, birefringence=birefringence, omit_zeros=True)
-    
+    print('Done!')
     # open up a new viewer if we dont have a current viewer
     if viewer is None:
+        print('Creating a new viewer')
         viewer = open_viewer()
     
     # set the
@@ -200,17 +233,50 @@ def plot_sliced_vectors(birefringence, optic_axis, viewer = None, colorlims = No
     else:
         raise TypeError(f"um_per_pix is niether a tuple with len 3 or a float or int. It is a {type(um_per_pix)} and I'm not sure what to do with this")
 
+    print('Adding vectors to the viewer')
     sliced_vectors = viewer.add_vectors(sliced_vects,features=sliced_bir,edge_color='bir',vector_style='line',scale=scale4, edge_contrast_limits=colorlims, edge_colormap=vs_cmap, edge_width=.3,length=1,opacity=1,blending='opaque')
-    return sliced_vectors
+    return viewer, sliced_vectors
 
+def viz_vol(volume,viewer = None):
+    birefringence,optic_axis,um_per_pix = load_np_from_birVol(volume)
+    optic_axis = bir_threshold(optic_axis,birefringence)
+    viewer,all = plot_all_vectors(birefringence,optic_axis,viewer=viewer,um_per_pix=um_per_pix)
+    napari.run()
+    return viewer, all
 
 if __name__ == '__main__':
-    birefringence, optic_axis = load_from_numpy(r"C:\Users\trevo\Desktop\celery_30_60_140_from2024-05-15_17-40-05_random1.npz")
-    print(birefringence.shape)
-    print(optic_axis.shape)
-    optic_axis = bir_threshold(optic_axis,birefringence)
-    plot = plot_sliced_vectors(birefringence, optic_axis)
-    napari.run()
+    # birefringence, optic_axis = load_from_npz(r"C:\Users\trevo\Desktop\celery_30_60_140_from2024-05-15_17-40-05_random1.npz")
+    # print(birefringence.shape)
+    # print(optic_axis.shape)
+    # optic_axis = bir_threshold(optic_axis,birefringence)
+    # viewer,sliced = plot_sliced_vectors(birefringence, optic_axis)
+    # _,all = plot_all_vectors(birefringence,optic_axis,viewer=viewer)
+    # napari.run()
+    
+    # %% Importing necessary libraries
+    import time
+    from VolumeRaytraceLFM.abstract_classes import BackEnds
+    from VolumeRaytraceLFM.simulations import ForwardModel
+    from VolumeRaytraceLFM.birefringence_implementations import BirefringentVolume
+    from VolumeRaytraceLFM.volumes import volume_args
+    from VolumeRaytraceLFM.setup_parameters import setup_optical_parameters
+    from VolumeRaytraceLFM.visualization.plotting_volume import visualize_volume
+
+    # %% Setting up the optical system
+    BACKEND = BackEnds.PYTORCH
+    optical_info = setup_optical_parameters("config/optical_config_sphere.json")
+    optical_system = {"optical_info": optical_info}
+    print(optical_info)
+
+    # %% Create the simulator and volume
+    volume = BirefringentVolume(
+        backend=BACKEND,
+        optical_info=optical_info,
+        volume_creation_args=volume_args.shell_small_args,
+    )
+    # plotly_figure = volume.plot_lines_plotly(draw_spheres=True)
+    # plotly_figure.show()
+    viz_vol(volume)
 
 
 
