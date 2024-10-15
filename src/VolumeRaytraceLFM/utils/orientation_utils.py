81589f0e7175f53069347718f9cea38e7ab0f3a2
Geneva Schlafly <gschlafly@uchicago.edu>
2024-10-15 12:56:06 -0500

    Undo transpose and flip correctly

--- src/VolumeRaytraceLFM/utils/orientation_utils.py
+++ src/VolumeRaytraceLFM/utils/orientation_utils.py
@@ -14,9 +14,9 @@ def transpose_and_flip(data):
 
 def undo_transpose_and_flip(data):
     if isinstance(data, np.ndarray):
-        result = np.flip(data.T, axis=1).copy()
+        result = np.flip(data, axis=1).T.copy()
     elif isinstance(data, torch.Tensor):
-        result = torch.flip(data.transpose(0, 1), dims=[1]).clone()
+        result = torch.flip(data, dims=[1]).transpose(0, 1).clone()
     else:
         raise TypeError("Input must be either a NumPy array or PyTorch tensor.")
     return result
